cfb4e1e3fc10c9a46b8615e99beba8d463187b15
heekang93 <153481850+heekang93@users.noreply.github.com>
2024-03-11 11:41:45 +0900

    엄희강
    홈페이지 CSS JS IMG common 파일에 업로드

---
+++ src/main/webapp/resources/js/엄희강.js
@@ -0,0 +1,37 @@
+
+    //사이드바 스크롤 이벤트
+    $(document).ready(function(){
+        var sidebar = $(".sidebar");
+        var footerBottom = $(".sidebarEnd").offset().top + $(".sidebarEnd").outerHeight(); 
+       
+        
+        $(window).scroll(function() {
+            var position = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            
+            var mainContentOffset = $(".main_content").offset().top;
+            var sidebarHeight = sidebar.outerHeight();
+            var endOffset = footerBottom - sidebarHeight - 20; 
+        
+            if (position >= mainContentOffset && position + windowHeight < endOffset) {
+                sidebar.stop().animate({"top": position + "px"}, 600);
+            } else if (position + windowHeight >= endOffset) {
+                sidebar.stop().animate({"top": endOffset - sidebarHeight + "px"}, 600);
+            } else {
+                sidebar.stop().animate({"top": mainContentOffset + "px"}, 600);
+            }
+        });
+    });
+
+
+
+    //사이드바 클릭 이벤트 
+    function scrollToTop() {
+        window.scrollTo(0, 0);
+    }
+    function scrollToBottom() {
+        window.scrollTo(0,document.body.scrollHeight);
+
+    }
+
+    
