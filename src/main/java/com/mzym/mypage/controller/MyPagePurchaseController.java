f230406c94ca51c6fa9866f3d9c670b80c723d52
sonsuhyeon <153481647+sonsuhyeon@users.noreply.github.com>
2024-03-15 12:07:52 +0900

    손수현 / add servlet, jsp
    add com.mzym.mypage.controller.MyPageDeleteFromController
    
    add com.mzym.mypage.controller.pageInbodyController
    
    add com.mzym.mypage.controller.MyPagePurchaseController
    
    add views/common/mypage_sidemenu.jsp
    
    add views/mypage/inbodyInfo.jsp
    add views/mypage/purchaseHistory.jsp

---
+++ src/main/java/com/mzym/mypage/controller/MyPagePurchaseController.java
@@ -0,0 +1,66 @@
+package com.mzym.mypage.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mzym.common.paging.PageInfo;
+import com.mzym.mypage.service.MyPageService;
+
+/**
+ * Servlet implementation class MyPagePurchaseController
+ */
+@WebServlet("/purchase.me")
+public class MyPagePurchaseController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyPagePurchaseController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int listCount;
+		int currentPage;
+		int pageLimit;
+		int boardLimit;
+		int maxPage;
+		int startPage;
+		int endPage;
+		
+		listCount = new MyPageService().selectListCount();
+		currentPage = Integer.parseInt(request.getParameter("page"));
+		pageLimit = 5;
+		boardLimit = 10;
+		maxPage = (int)Math.ceil((double)listCount / boardLimit);
+		startPage = (currentPage-1) / pageLimit * pageLimit + 1;
+		endPage = startPage + pageLimit -1;
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		PageInfo pi = new PageInfo(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage, endPage);
+		List<Purchase>list = new MyPageService().selectList(pi);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
