0281f875cdf2ad150b8013a0f4f8ad104145ba78
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2024-03-19 15:53:20 +0900

    김민정/CalendarList

--- src/main/java/com/mzym/calendar/dao/CalendarDao.java
+++ src/main/java/com/mzym/calendar/dao/CalendarDao.java
@@ -3,11 +3,15 @@ package com.mzym.calendar.dao;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.InvalidPropertiesFormatException;
+import java.util.List;
 import java.util.Properties;
 
 import com.mzym.calendar.vo.Calendar;
+import com.mzym.member.model.vo.Member;
 import static com.mzym.common.template.JDBCTemplate.*;
 
 public class CalendarDao {
@@ -17,7 +21,7 @@ public class CalendarDao {
 	public CalendarDao() {
 		
 		try {
-			prop.loadFromXML(CalendarDao.class.getResourceAsStream("/db/mappers/calendar-maper.xml"));
+			prop.loadFromXML(CalendarDao.class.getResourceAsStream("/db/mappers/calendar-mapper.xml"));
 		} catch (InvalidPropertiesFormatException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -25,6 +29,65 @@ public class CalendarDao {
 		}
 		
 	}
+	
+	public List<Calendar> selectCalendarList(Connection conn, int userNo){
+		List<Calendar> clist = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectCalendarList");
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, userNo);
+			
+			rset = pstmt.executeQuery();
+			while(rset.next()) {
+				clist.add(new Calendar(rset.getString("CAL_TR"),
+									   rset.getString("START_DATE"),
+									   rset.getString("END_DATE"),
+									   rset.getString("CAL_TITLE"),
+									   rset.getString("CAL_CONTENT"),
+									   rset.getString("CAL_COLOR")));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rset);
+			close(pstmt);
+		}
+		return clist;
+		
+	}
+	
+	/*
+	// 사용x
+	public List<Member> selelctMemberList(Connection conn, String userId){
+		List<Member> mlist = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectCalendarList");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				mlist.add(new Member(rset.getString("USER_NAME"),
+									 rset.getString("PHONE")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return mlist;
+		
+	}
+	
+	
+	
 	
 	
 	
@@ -56,7 +119,7 @@ public class CalendarDao {
 		
 	}
 	
-	
+	*/
 	
 	
 	
--- src/main/java/com/mzym/calendar/service/CalendarService.java
+++ src/main/java/com/mzym/calendar/service/CalendarService.java
@@ -1,15 +1,43 @@
 package com.mzym.calendar.service;
 
-import static com.mzym.common.template.JDBCTemplate.*;
+import static com.mzym.common.template.JDBCTemplate.close;
+import static com.mzym.common.template.JDBCTemplate.commit;
+import static com.mzym.common.template.JDBCTemplate.getConnection;
+import static com.mzym.common.template.JDBCTemplate.rollback;
 
 import java.sql.Connection;
+import java.util.List;
 
 import com.mzym.calendar.dao.CalendarDao;
 import com.mzym.calendar.vo.Calendar;
+import com.mzym.member.model.vo.Member;
 
 public class CalendarService {
 	
 	CalendarDao cDao = new CalendarDao();
+	
+	
+	public List<Calendar> selectCalendarList(int userNo){
+		
+		Connection conn = getConnection();
+		List<Calendar> clist = cDao.selectCalendarList(conn, userNo);
+		close(conn);
+		return clist;
+		
+	}
+	
+	
+	/*
+	// 사용 x
+	public List<Member> selelctMemberList(String userId){
+		Connection conn = getConnection();
+		List<Member> mlist = cDao.selelctMemberList(conn, userId);
+		close(conn);
+		return mlist;
+	}
+	
+	
+	
 	
 	
 	public int calInsert(Calendar cal, String userName, String phone) {
@@ -26,6 +54,6 @@ public class CalendarService {
 		close(conn);
 		return result;
 	}
-	
+	*/
 
 }
