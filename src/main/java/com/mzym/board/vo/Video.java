6ec0fe5e6caae3d6f168cfd284129c4625bb0d18
sonsuhyeon <153481647+sonsuhyeon@users.noreply.github.com>
2024-03-22 12:39:09 +0900

    손수현 / video

--- src/main/java/com/mzym/board/vo/Video.java
+++ src/main/java/com/mzym/board/vo/Video.java
@@ -7,30 +7,23 @@ public class Video {
 	private String link;
 	private String staus;
 	private int videoLevel;
-	private int count;
 	
-	
-
-	public Video(int videoNo, String videoTitle, String link, String staus, int videoLevel, int count) {
+	public Video(int videoNo, String videoTitle, String link, String staus, int videoLevel) {
 		super();
 		this.videoNo = videoNo;
 		this.videoTitle = videoTitle;
 		this.link = link;
 		this.staus = staus;
 		this.videoLevel = videoLevel;
-		this.count = count;
 	}
 
-	public Video(int videoNo, String videoTitle, String link, int videoLevel, int count) {
+	public Video(int videoNo, String videoTitle, String link, int videoLevel) {
 		super();
 		this.videoNo = videoNo;
 		this.videoTitle = videoTitle;
 		this.link = link;
 		this.videoLevel = videoLevel;
-		this.count = count;
 	}
-
-
 
 	public int getVideoNo() {
 		return videoNo;
@@ -72,21 +65,16 @@ public class Video {
 		this.videoLevel = videoLevel;
 	}
 
-	public int getCount() {
-		return count;
-	}
-
-	public void setCount(int count) {
-		this.count = count;
-	}
-
 	@Override
 	public String toString() {
 		return "Video [videoNo=" + videoNo + ", videoTitle=" + videoTitle + ", link=" + link + ", staus=" + staus
-				+ ", videoLevel=" + videoLevel + ", count=" + count + "]";
+				+ ", videoLevel=" + videoLevel + "]";
 	}
+	
+	
+	
 
-
+	
 	
 	
 	
