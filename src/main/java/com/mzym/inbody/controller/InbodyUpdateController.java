c68405b84d5e24303cfc046c9e858864d0bc057f
hwangsurim <153481548+hwangsurim@users.noreply.github.com>
2024-03-27 18:26:53 +0900

    0327 황수림 인바디

--- src/main/java/com/mzym/inbody/controller/InbodyUpdateController.java
+++ src/main/java/com/mzym/inbody/controller/InbodyUpdateController.java
@@ -57,7 +57,7 @@ public class InbodyUpdateController extends HttpServlet {
 			response.sendRedirect(request.getContextPath() + "/indobyForm.trainar");
 			
 		}else {
-			// 에러페이지
+			// 에러페이지.
 		}
 		
 	}
